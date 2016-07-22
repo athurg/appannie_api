142b6b5ae5a783c6193e01c4d3a466d574f30687
Athurg Gooth <athurg@gooth.org>
2016-07-22 18:41:57 +0800

    Add Accounts api

---
+++ app_analytics.go
@@ -0,0 +1,33 @@
+package appannie
+
+import (
+	"net/url"
+)
+
+type AccountInfo struct {
+	AccountId      int `json:"account_id"`
+	Vertical       string
+	Market         string
+	AccountName    string `json:"account_name"`
+	PublisherName  string `json:"publisher_name"`
+	FirstSalesDate string `json:"first_sales_date"`
+	LastSalesDate  string `json:"last_sales_date"`
+	AccountStatus  string `json:"account_status"`
+}
+
+//TODO:for large responses, need to merge all pages
+func (cli *Client) Accounts() ([]AccountInfo, error) {
+	var resp struct {
+		APIResponse
+		PagedAPIResponse
+		Accounts []AccountInfo
+	}
+
+	q := url.Values{"page_index": []string{"0"}}
+	err := cli.request("/accounts", q, &resp)
+	if err != nil {
+		return nil,err
+	}
+
+	return resp.Accounts, err
+}
